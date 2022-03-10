69653fb54793e3af19d5ef3c57c7a507a533504e
fu <1181488349@qq.com>
2022-03-10 23:52:10 +0800

    '3.10'

---
+++ day-3/schema/schema.go
@@ -0,0 +1,51 @@
+package schema
+
+import (
+	"geeorm/dialect"
+	"go/ast"
+	"reflect"
+)
+
+type Field struct {
+	Name string
+	Type string
+	Tag string
+}
+
+type Schema struct {
+	Model interface{}
+	Name string
+	Fields []*Field
+	FieldNames []string
+	fieldMap map[string]*Field
+}
+
+func (s *Schema) GetField(name string) *Field {
+	return s.fieldMap[name]
+}
+
+func Parse(dest interface{}, d dialect.Dialect) *Schema {
+	modeType := reflect.Indirect(reflect.ValueOf(dest)).Type()
+	schema := &Schema{
+		Model: dest,
+		Name: modeType.Name(),
+		fieldMap: make(map[string]*Field),
+	}
+
+	for i := 0; i < modeType.NumField(); i++ {
+		p := modeType.Field(i)
+		if !p.Anonymous && ast.IsExported(p.Name) {
+			field := &Field{
+				Name: p.Name,
+				Type: d.DataTypeOf(reflect.Indirect(reflect.New(p.Type))),
+			}
+			if v,ok := p.Tag.Lookup("geeorm"); ok {
+				field.Tag = v
+			}
+			schema.Fields = append(schema.Fields, field)
+			schema.FieldNames = append(schema.FieldNames, p.Name)
+			schema.fieldMap[p.Name] = field
+		}
+	}
+	return schema
+}
