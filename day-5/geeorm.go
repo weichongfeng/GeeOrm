57f23dae7cc0581409b230a899e7005e6b97098f
fu <1181488349@qq.com>
2022-03-15 21:39:55 +0800

    'hooks'

---
+++ day-5/geeorm.go
@@ -0,0 +1,48 @@
+package geeorm
+
+import (
+	"database/sql"
+	"geeorm/dialect"
+	"geeorm/log"
+	"geeorm/session"
+	_ "github.com/mattn/go-sqlite3"
+)
+
+type Engine struct {
+	db *sql.DB
+	dialect dialect.Dialect
+}
+
+func NewEngine(driver, source string) (e *Engine, err error) {
+	db, err := sql.Open(driver, source)
+	if err != nil {
+		log.Error(err)
+		return
+	}
+
+	if err = db.Ping(); err != nil {
+		log.Error(err)
+		return
+	}
+
+	dial, ok := dialect.GetDialect(driver)
+	if !ok {
+		log.Error("dialect %s Not Found", driver)
+		return
+	}
+
+	e = &Engine{db: db, dialect: dial}
+	log.Info("Connect database success")
+	return
+}
+
+func (e *Engine) Close() {
+	if err := e.db.Close(); err != nil {
+		log.Error("Failed to close database")
+	}
+	log.Info("Close database success")
+}
+
+func (e *Engine) NewSession() *session.Session {
+	return session.New(e.db,e.dialect)
+}
